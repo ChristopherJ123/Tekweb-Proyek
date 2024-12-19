629f4c0930b86f7ca862be64bf4a9d1f48ac931f
miche <michellenathaniawinata@gmail.com>
2024-12-19 08:18:01 +0700

    Working private chat

--- public/chat.php
+++ public/chat.php
@@ -3,6 +3,13 @@ include "../src/db.php";
 global $conn;
 session_start();
 
+if (!isset($_SESSION['user_id'])) {
+    $errors = [];
+    $errors[] = "Silahkan login terlebih dahulu";
+    $_SESSION['errors'] = $errors;
+    header("Location: login.php");
+    exit();
+}
 ?>
 
 <!doctype html>
@@ -15,6 +22,7 @@ session_start();
     <title>Chat | PasarKakiLima</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <style>
         body {
             background-color: white;
@@ -23,19 +31,24 @@ session_start();
             background-color: white;
             box-shadow: 0 4px 30px lightgray;
             border-radius: 16px;
-            height: 100%;
+            min-height: 90vh;
             margin: 2em;
+            padding-bottom: 2em;
         }
         .left-side {
             width: 40%;
             padding: 0em;
         }
         .right-side {
+            position: relative;
             width: 60%;
             border-left: 1px solid lightgray;
         }
 
         .profile-circle {
+            display: flex;
+            justify-content: center;
+            align-items: center;
             width: 50px;
             height: 50px;
             border-radius: 50%;
@@ -68,17 +81,15 @@ session_start();
             align-items: start;
 
             width: 100%;
-            height: 6vh;
-            background-color: lime;
-            margin: 0em;
+            margin: 0.4em 0em 0em 0em;
         }
 
-        .receiver-message-box {
+        .user-message-box {
             display: flex;
             justify-content: end;
         }
 
-        .receiver-message {
+        .user-message {
             display: flex;
             justify-content: end;
 
@@ -91,12 +102,12 @@ session_start();
             margin: 10px;
         }
 
-        .sender-message-box {
+        .target-message-box {
             display: flex;
             justify-content: start;
         }
 
-        .sender-message {
+        .target-message {
             display: flex;
             justify-content: end;
 
@@ -108,6 +119,84 @@ session_start();
             padding: 10px 10px 0px 10px;
             margin: 10px;
         }
+
+        .input-message-box {
+            position: absolute;
+            width: 100%;
+            bottom: 0;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-top: 10px;
+            padding-right: 10px;
+        }
+
+        .input-message {
+            display: flex;
+            justify-content: start;
+            align-items: center;
+            float: left;
+            resize: none;
+            min-height: 44px;
+
+            width: calc(100% - 100px);
+            /*max-width: 1000px;*/
+            height: 5vh;
+            font: medium Verdana;
+            border-radius: 16px;
+            border: 2px solid gray;
+            padding: 10px;
+            margin: 0em 0.2em 0em 0em;
+        }
+
+        .send-button {
+            float: right;
+            background-color: lawngreen;
+            width: 45px;
+            height: 45px;
+            border-radius: 16px;
+            /*padding: 0.3em;*/
+            /*margin: 0.2em 0.4em 0.2em 0.6em;*/
+        }
+
+        /* Responsiveness */
+        @media (max-width: 480px) {
+            .user-message {
+                font: small Verdana;
+            }
+            .target-message {
+                font: small Verdana;
+            }
+            small, em {
+                font: small Verdana;
+            }
+            .profile-circle {
+                width: 35px;
+                height: 35px;
+            }
+            .profile-circle img {
+                width: 24px;
+            }
+            .chat-profile-stack {
+                height: 48px;
+            }
+            .input-message {
+                width: 86%;
+                height: 5vh;
+                font: small Verdana;
+                min-height: 24px;
+            }
+
+            .send-button {
+                float: right;
+                background-color: lawngreen;
+                width: 45px;
+                height: 45px;
+                border-radius: 16px;
+                /*padding: 0.3em;*/
+                /*margin: 0.2em 0.4em 0.2em 0.6em;*/
+            }
+        }
     </style>
 </head>
 <body>
@@ -116,65 +205,92 @@ session_start();
             <h2 class="fs-2 fw-bold mx-4 pt-3">Chat</h2>
 
             <div class="chat-profile-stacks">
-                <div class="chat-profile-stack">
-                    <a class="profile-circle">
-                        <img src="assets/Bootstrap-person-fill-icon.png" alt="">
-                    </a>
-                    <span class="d-flex flex-column">
-                        <small class="fs-5">John Doe</small>
-                        <em class="fst-italic">seen</em>
-                    </span>
-                </div>
-                <div class="chat-profile-stack">
-                    <a class="profile-circle">
-                        <img src="assets/Bootstrap-person-fill-icon.png" alt="">
-                    </a>
-                    <span class="d-flex flex-column">
-                        <small class="fs-5">John Doe</small>
-                        <em class="fst-italic">seen</em>
-                    </span>
-                </div>
-                <div class="chat-profile-stack">
-                    <a class="profile-circle">
-                        <img src="assets/Bootstrap-person-fill-icon.png" alt="">
-                    </a>
-                    <span class="d-flex flex-column">
-                        <small class="fs-5">John Doe</small>
-                        <em class="fst-italic">seen</em>
-                    </span>
-                </div>
-
+                <?php
+                $userID = $_SESSION['user_id'];
+                $queryGetTargetID = "
+                    SELECT DISTINCT users.id, users.username, users.profile_picture FROM private_chats
+                    JOIN users ON private_chats.target_id = users.id
+                    WHERE user_id = '$userID'
+                ";
+                $result = mysqli_query($conn, $queryGetTargetID);
+                    foreach ($result as $row) { ?>
+                        <div onclick="location.href='chat.php?target=<?=$row['id']?>'" class="chat-profile-stack">
+                            <span class="profile-circle">
+                                <img src="<?=$row['profile_picture']?>" alt="pp">
+                            </span>
+                            <span class="d-flex flex-column">
+                                <small><?=$row['username']?></small>
+                                <em class="fst-italic">seen</em>
+                            </span>
+                        </div>
+                    <?php } ?>
             </div>
         </div>
 
         <div class="right-side">
-            <div class="chat-profile-page">
-                <a class="profile-circle">
-                    <img src="assets/Bootstrap-person-fill-icon.png" alt="">
-                </a>
-                <span class="d-flex flex-column">
-                        <small class="fs-5">John Doe</small>
+            <?php
+            if (isset($_GET['target'])) {
+                $targetID = $_GET['target'];
+                $queryGetTargetID = "
+                SELECT users.username, users.profile_picture FROM users
+                WHERE id = '$targetID'";
+                $result = mysqli_query($conn, $queryGetTargetID);
+                $row = mysqli_fetch_assoc($result);
+                ?>
+                    <div class="chat-profile-page">
+                        <a class="profile-circle">
+                            <img src="<?=$row['profile_picture']?>" alt="">
+                        </a>
+                        <span class="d-flex flex-column">
+                        <small><?=$row['username']?></small>
                         <em class="fst-italic">seen</em>
-                    </span>
-            </div>
+                        </span>
+                    </div>
+                <?php
+            }
+            ?>
 
-            <div class="receiver-message-box">
-                <div class="receiver-message">
-                    <p>Halo kaka saya mau permen</p>
+            <?php
+            if (isset($_GET['target'])) {
+                $userID = $_SESSION['user_id'];
+                $targetID = $_GET['target'];
+                $queryGetContent = "SELECT private_chats.user_id, private_chats.target_id, private_chats.id, private_chats.content FROM private_chats
+                                    WHERE (user_id = '$userID' AND target_id = '$targetID') OR (user_id = '$targetID' AND target_id = '$userID')
+                                    ORDER BY private_chats.created_at ASC";
+                $result = mysqli_query($conn, $queryGetContent);
+                foreach ($result as $row) {
+                    if ($row['user_id'] == $userID) { ?>
+                        <div class="user-message-box">
+                            <div class="user-message">
+                                <p><?=$row['content']?></p>
+                            </div>
+                        </div>
+                    <?php }
+                    else { ?>
+                        <div class="target-message-box">
+                            <div class="target-message">
+                                <p><?=$row['content']?></p>
+                            </div>
+                        </div>
+                    <?php }
+                } ?>
+                <div class="input-message-box">
+                    <form action="./scripts/chat_script.php" method="POST" style="width: 100%;">
+                        <input type="hidden" name="targetUserID" value="<?=$targetID?>">
+                        <label>
+                            <textarea type="text" name="content" class="input-message" placeholder="Message.."></textarea>
+                        </label>
+                        <button class="btn send-button" type="submit">
+                            <i class="bi bi-send"></i>
+                        </button>
+                    </form>
                 </div>
-            </div>
+                <?php
+            }
+            ?>
 
-            <div class="sender-message-box">
-                <div class="sender-message">
-                    <p>Ready kak. Bisa langsung order saja ya di etalase 2 kami. Buruan checkout sebelum kehabisan!!!</p>
-                </div>
-            </div>
 
-            <div class="receiver-message-box">
-                <div class="receiver-message">
-                    <p>Santai gan 😭😭</p>
-                </div>
-            </div>
+
 
         </div>
 
--- public/scripts/chat_script.php
+++ public/scripts/chat_script.php
@@ -1,5 +1,35 @@
 <?php
+session_start();
+include '../../src/db.php';
+global $conn;
 
-//if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//
-//}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $errors = [];
+
+    if (isset($_SESSION['user_id'])) {
+        if (isset($_POST["content"]) && isset($_POST["targetUserID"])) {
+            $userID = $_SESSION['user_id'];
+            $message = $_POST["content"];
+            $targetUserID = $_POST["targetUserID"];
+
+            $queryInsert = "INSERT INTO private_chats (user_id, target_id, content) VALUES ('$userID', '$targetUserID', '$message')";
+            if (mysqli_query($conn, $queryInsert)) {
+                mysqli_close($conn);
+                header('Location: ../chat.php?target='.$targetUserID);
+                exit();
+            }
+
+        } else {
+            $errors[] = "An error has occurred!";
+        }
+    } else {
+        $errors[] = "You need to login first!";
+    }
+
+    if (!empty($errors)) {
+        $_SESSION['errors'] = $errors;
+        header("Location: ../chat.php");
+        exit();
+    }
+}
+?>
